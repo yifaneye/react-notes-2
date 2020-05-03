716bf8175b72c2b48b1b5d2184673ec84345fc0a
Yifan Ai <ayfallen@hotmail.com>
2020-05-04 01:55:06 +1000

    Pass in component properties props

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ var style = {
 class Message extends React.Component {
 	render() {
 		return (
-			<div style={style}>
-				<h1>Hello Again</h1>
-				<p>I am still Allen</p>
+			<div style={{color: this.props.color}}>
+				<h1>Hello {this.props.name}</h1>
+				<p>I am still Allen {this.props.number}</p>
 			</div>
 		)
 	}
 }
 
 ReactDOM.render(
-	<Message />,
+	<Message color="blue" name="Bob" number={3} />,
 	document.getElementById('root')
 );
