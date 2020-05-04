75b7ddba630374b39fd0def5be374e02a045c279
Yifan Ai <ayfallen@hotmail.com>
2020-05-04 17:08:35 +1000

    Use REST API to mount component

--- src/index.js
+++ src/index.js
@@ -32,7 +32,18 @@ class Store extends Component {
 	state = {
 		open: true,
 		free: true,
+		data: [],
+		loading: false
 	};
+	componentDidMount() {
+		this.setState({loading: true});
+		fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/3')
+			.then(data => data.json())
+			.then(data => this.setState({data, loading: false}))
+	}
+	componentDidUpdate(prevProps, prevState, snapshot) {
+	}
+
 	toggleOpenness = () => {
 		this.setState(prevState => ({
 			open: !prevState.open
@@ -45,6 +56,18 @@ class Store extends Component {
 			<div>
 				<h1>iBook Store is <strong onClick={this.toggleOpenness}>{this.state.open ? 'open' : 'closed'}</strong></h1>
 				{this.state.open ? <CloseTime /> : <OpenTime />}
+				{this.state.loading ? "loading ..." :
+					<div>{this.state.data.map(product => {
+					return (
+						<div key={product.id}>
+							<h3>Special</h3>
+							<h4>{product.name}</h4>
+							<img src={product.image} alt={product.name} height={100} />
+						</div>
+					)
+				})}
+						</div>
+				}
 				{books.map(
 					(book, i) => <Book key={i} title={book.title} nPages={book.nPages} author={book.author} free={this.state.free}/>
 				)}
